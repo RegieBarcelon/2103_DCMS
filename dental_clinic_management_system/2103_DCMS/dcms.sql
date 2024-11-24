bd001c71d63ecd2d18459761b4236b1beb515ed0
RegieBarcelon <regiebarcelon@gmail.com>
2024-11-24 17:45:56 +0800

    Add files via upload

---
+++ dental_clinic_management_system/2103_DCMS/dcms.sql
@@ -0,0 +1,244 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 23, 2024 at 07:29 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dcms`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `appointment`
+--
+
+CREATE TABLE `appointment` (
+  `First Name` varchar(50) NOT NULL,
+  `Last Name` varchar(50) NOT NULL,
+  `Age` int(100) NOT NULL,
+  `Gender` varchar(2) NOT NULL,
+  `Date of Birth` varchar(50) NOT NULL,
+  `Address` varchar(50) NOT NULL,
+  `Contact` int(12) NOT NULL,
+  `Email` varchar(50) NOT NULL,
+  `Appointment date` date NOT NULL,
+  `Appointment time` time(6) NOT NULL,
+  `Treatment` varchar(50) NOT NULL,
+  `Dentist` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bill`
+--
+
+CREATE TABLE `bill` (
+  `Datee` date NOT NULL,
+  `Time` time(6) NOT NULL,
+  `RECEIPT NUMBER` int(50) NOT NULL,
+  `No` int(50) NOT NULL,
+  `Name` varchar(50) NOT NULL,
+  `treatment` varchar(50) NOT NULL,
+  `Amount` int(50) NOT NULL,
+  `Payment Method` varchar(50) NOT NULL,
+  `Amount Paid` int(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `history`
+--
+
+CREATE TABLE `history` (
+  `Petya` date NOT NULL,
+  `Patient` varchar(50) NOT NULL,
+  `Treatmentt` varchar(50) NOT NULL,
+  `Dentist` varchar(50) NOT NULL,
+  `Status` varchar(50) NOT NULL,
+  `Remarks` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `loginpage`
+--
+
+CREATE TABLE `loginpage` (
+  `UserName` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `prescription`
+--
+
+CREATE TABLE `prescription` (
+  `Date` date NOT NULL,
+  `Time` time(6) NOT NULL,
+  `Patient Name` varchar(50) NOT NULL,
+  `Contact Number` int(50) NOT NULL,
+  `Type of Treatment` varchar(50) NOT NULL,
+  `Dentist Assigned` varchar(50) NOT NULL,
+  `Remarks` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `registration`
+--
+
+CREATE TABLE `registration` (
+  `ID` int(50) NOT NULL,
+  `Username` varchar(50) NOT NULL,
+  `Password` varchar(50) NOT NULL,
+  `Email` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `registration`
+--
+
+INSERT INTO `registration` (`ID`, `Username`, `Password`, `Email`) VALUES
+(1, 'Regie', 'Regie@gmail.com', '12345');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `scedule`
+--
+
+CREATE TABLE `scedule` (
+  `PatientID` int(11) NOT NULL,
+  `DATE` date NOT NULL,
+  `TIME` time(6) NOT NULL,
+  `PATIENT` varchar(50) NOT NULL,
+  `TREATMENT` varchar(50) NOT NULL,
+  `DENTIST` varchar(50) NOT NULL,
+  `STATUS` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `treatment`
+--
+
+CREATE TABLE `treatment` (
+  `Name` varchar(50) NOT NULL,
+  `Address` varchar(50) NOT NULL,
+  `Contact` int(50) NOT NULL,
+  `Age` int(50) NOT NULL,
+  `Date of Birth` int(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `appointment`
+--
+ALTER TABLE `appointment`
+  ADD PRIMARY KEY (`Age`);
+
+--
+-- Indexes for table `bill`
+--
+ALTER TABLE `bill`
+  ADD PRIMARY KEY (`Datee`);
+
+--
+-- Indexes for table `history`
+--
+ALTER TABLE `history`
+  ADD PRIMARY KEY (`Petya`);
+
+--
+-- Indexes for table `loginpage`
+--
+ALTER TABLE `loginpage`
+  ADD PRIMARY KEY (`UserName`);
+
+--
+-- Indexes for table `prescription`
+--
+ALTER TABLE `prescription`
+  ADD PRIMARY KEY (`Contact Number`);
+
+--
+-- Indexes for table `registration`
+--
+ALTER TABLE `registration`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `scedule`
+--
+ALTER TABLE `scedule`
+  ADD PRIMARY KEY (`PatientID`);
+
+--
+-- Indexes for table `treatment`
+--
+ALTER TABLE `treatment`
+  ADD PRIMARY KEY (`Contact`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `appointment`
+--
+ALTER TABLE `appointment`
+  MODIFY `Age` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `prescription`
+--
+ALTER TABLE `prescription`
+  MODIFY `Contact Number` int(50) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `registration`
+--
+ALTER TABLE `registration`
+  MODIFY `ID` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `scedule`
+--
+ALTER TABLE `scedule`
+  MODIFY `PatientID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `treatment`
+--
+ALTER TABLE `treatment`
+  MODIFY `Contact` int(50) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
